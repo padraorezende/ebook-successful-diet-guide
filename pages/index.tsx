aa53a5b24b6bfc74685d1838252f2c27e8439883
lucas_padrao <lucas_padrao@microuniverso.com.br>
2023-04-17 22:04:50 -0300

    mudancas layout

--- pages/index.tsx
+++ pages/index.tsx
@@ -23,28 +23,41 @@ export default function Home() {
         <link rel="icon" href="/favicon.png" />
       </Head>
 
-      <header className="w-full shadow-md">
+      {/* <header className="w-full shadow-md bg-teal-600">
         <div className="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8 flex flex-col md:flex-row justify-between items-center">
           <div className="text-center md:text-left mb-2 md:mb-0">
-            <h1 className={`text-3xl font-bold leading-tight ${inter.className}`} >
+            <h1 className={`text-4xl font-bold leading-tight ${inter.className}`} >
               O GUIA IDEAL PARA UMA DIETA DE SUCESSO:
             </h1>
             <div className="inline-flex items-center">
-              <h2 className={`text-xl font-bold leading-tight ${inter.className}`} >
+              <h2 className={`text-3xl font-bold leading-tight ${inter.className}`} >
                 Saiba como alcançar seus objetivos
               </h2>
               <img src="/images/objective.png" alt="Stripe" className="w-8 ml-2" />
             </div>
           </div>
-          <a href="#" className="text-blue-600 hover:text-blue-800 font-medium hidden sm:block">
+          <a href="#" className="text-blue-600 hover:text-blue-800 font-medium hidden">
             Ver outros produtos
           </a>
         </div>
-      </header>
+      </header> */}
+
+      <div className="text-center mt-14 mb-2 md:mb-0">
+        <h1 className={`text-5xl font-bold leading-tight ${inter.className}`} >
+          O GUIA IDEAL PARA UMA DIETA DE SUCESSO:
+        </h1>
+        <div className="inline-flex items-center">
+          <h2 className={`text-4xl font-bold leading-tight ${inter.className}`} >
+            Saiba como alcançar seus objetivos
+          </h2>
+          <img src="/images/objective.png" alt="Stripe" className="w-8 ml-2" />
+        </div>
+      </div>
 
       <div className="py-12 h-full flex-col text-center">
-        <h2 className="text-xl font-semibold text-gray-800 mb-8">
-          Aprenda a alcançar seus objetivos com o nosso guia completo
+        <h2 className="text-xl font-bold text-gray-800 mb-12">
+          Programa de Reeducação Alimentar Ajuda <div className="text-xl  text-red-600">Mais de 75.000 Pessoas em 7 Países nos Últimos 2 Anos</div>que
+          Precisavam Reduzir Drasticamente Suas <div className='inline-flex underline'>GORDURAS CORPORAIS</div> em Poucos Dias
         </h2>
 
         <div className="flex flex-col items-center justify-center mb-8">
@@ -53,9 +66,10 @@ export default function Home() {
               <source src="/videos/ebook.mp4" type="video/mp4" />
             </video>
           </div>
-          <div className="md:w-1/2 mt-8 mx-2 flex flex-col">
-            <button className="py-3 bg-blue-500 hover:bg-blue-600 text-white rounded-lg shadow-lg font-medium">
-              Finalizar minha Compra
+          <div className="md:w-1/2 mt-8 mx-2 flex flex-col items-center">
+            <button className="py-3 bg-lime-500 hover:bg-lime-600 text-white rounded-lg shadow-lg font-medium w-4/5"
+              onClick={() => window.location.href = "https://checkout.ticto.app/O34A48766"}>
+              SIM, EU QUERO PERDER <br /> GORDURA LOCALIZADA E EMAGRECER!
             </button>
             <div className="flex flex-row mt-4 items-center justify-center">
               <img src="/images/visa.png" alt="Visa" className="w-8 mr-4" />
@@ -97,13 +111,13 @@ export default function Home() {
           </h3>
 
           <div className="flex flex-col md:flex-row items-center justify-center">
-            <Card description='O guia de nutrição me ajudou a entender melhor como escolher os alimentos certos para cada refeição, e as receitas são incríveis!' location='São Paulo' name='João Silva' />
-            <Card description='Esse guia é incrível! As dicas práticas me ajudaram a montar meu cardápio semanal de forma equilibrada e variada.' location='Curitiba' name='Roberto Ferreira' />
-            <Card description='Com o guia de nutrição, aprendi a importância de cada grupo alimentar e como combiná-los para uma dieta saudável e saborosa.' location='Recife' name='Camila Lima' />
+            <Card description='"Segui o guia e perdi 3 quilos em uma semana, estou muito feliz. Conteúdo muito didático e simples de entender"' location='São Paulo' name='João Silva' />
+            <Card description='"Esse guia é incrível! As dicas práticas me ajudaram a montar meu cardápio semanal de forma equilibrada e variada"' location='Curitiba' name='Roberto Ferreira' />
+            <Card description='"Com o guia de nutrição, aprendi a importância de cada grupo alimentar e como combiná-los para uma dieta saudável e saborosa."' location='Recife' name='Camila Lima' />
           </div>
           <div className="flex flex-col md:flex-row items-center justify-center mt-4">
-            <Card description='Com o guia de nutrição, consegui alcançar meus objetivos de perda de peso de forma saudável e sem sacrifícios.' location='Rio de Janeiro' name='Maria Souza' />
-            <Card description='As informações do guia de nutrição são muito claras e objetivas, me ajudaram a desmistificar muitos mitos sobre alimentação.' location='Porto Alegre' name='Fernanda Oliveira' />
+            <Card description='"Com o guia de nutrição, consegui alcançar meus objetivos de perda de peso de forma saudável e sem sacrifícios"' location='Rio de Janeiro' name='Maria Souza' />
+            <Card description='"As informações do guia de nutrição são muito claras e objetivas, me ajudaram a desmistificar muitos mitos sobre alimentação."' location='Porto Alegre' name='Fernanda Oliveira' />
           </div>
 
         </div>
